57bc044772bd87711b6d2c8f9fc76139f040ea45
Aleksandr232 <aleksrowing232@gmail.com>
2026-01-22 20:16:26 +0300

    index

--- remission.php
+++ remission.php
@@ -13,7 +13,7 @@ require_once 'php/auth_check.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Библиотека Ремиссии - Lactofilitrum</title>
-    <link rel="stylesheet" href="css/admin.css?v=20241205">
+    <link rel="stylesheet" href="css/admin.css?v=<?php echo $timestamp; ?>">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
