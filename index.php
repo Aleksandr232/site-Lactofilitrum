160361c665610247703b917ec7a4161ba63d7a55
Aleksandr232 <aleksrowing232@gmail.com>
2026-02-04 19:40:53 +0300

    index

--- index.php
+++ index.php
@@ -17,6 +17,10 @@ try {
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="icon" type="image/x-icon" href="frontend/img/favicon/favicon.ico?v=<?php echo $timestamp; ?>" />
+		<link rel="icon" type="image/png" sizes="32x32" href="frontend/img/favicon/favicon-32x32.png?v=<?php echo $timestamp; ?>" />
+		<link rel="icon" type="image/png" sizes="16x16" href="frontend/img/favicon/favicon-16x16.png?v=<?php echo $timestamp; ?>" />
+		<link rel="apple-touch-icon" href="frontend/img/favicon/apple-touch-icon.png?v=<?php echo $timestamp; ?>" />
 		<link type="text/css" media="all" rel="stylesheet" href="frontend/css/swiper-bundle.min.css?v=<?php echo $timestamp; ?>" />
 		<link type="text/css" media="all" rel="stylesheet" href="frontend/css/choices.min.css?v=<?php echo $timestamp; ?>" />
 		<link type="text/css" media="all" rel="stylesheet" href="frontend/css/aos.css?v=<?php echo $timestamp; ?>" />
