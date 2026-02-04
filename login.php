2c837c9490bc8fa93ff16a0ed73e37234653ebbf
Aleksandr232 <aleksrowing232@gmail.com>
2026-02-04 19:44:15 +0300

    index

--- login.php
+++ login.php
@@ -7,6 +7,10 @@ $timestamp = time();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="frontend/img/favicon/favicon.ico?v=<?php echo $timestamp; ?>" />
+		<link rel="icon" type="image/png" sizes="32x32" href="frontend/img/favicon/favicon-32x32.png?v=<?php echo $timestamp; ?>" />
+		<link rel="icon" type="image/png" sizes="16x16" href="frontend/img/favicon/favicon-16x16.png?v=<?php echo $timestamp; ?>" />
+		<link rel="apple-touch-icon" href="frontend/img/favicon/apple-touch-icon.png?v=<?php echo $timestamp; ?>" />
     <title>Вход в админку - Lactofilitrum</title>
     <link rel="stylesheet" href="css/login.css?v=<?php echo $timestamp; ?>">
 </head>
